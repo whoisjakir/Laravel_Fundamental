c80df095d7269ac77b2f7ca1ae8061f65480ea78
Mohammad Jakir Hossain <jakir_comilla1@yahoo.com>
2023-02-08 16:08:47 +0600

    New View Update

--- Class_3_Create_New_Project_And_Run/blog/routes/web.php
+++ Class_3_Create_New_Project_And_Run/blog/routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Home', function () {
+    return view('HomePage');
+});
+
+
+Route::get('/hello',function(){
+    return "Hello World";
+});
